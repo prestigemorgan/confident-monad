615dae4479158e74782ddba9f6db9c3fa4484941
prestigemorgan <125128702+prestigemorgan@users.noreply.github.com>
2023-02-11 12:32:11 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import "./styles.css";
+import React from 'react';
+import './App.css';
+import { Route, Routes } from "react-router-dom"; 
+import {Home, About, Contact, Personal, Login, MyAccount, } from './Pages';
+import { Footer, Header } from './Components';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+     <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/personal" element={<Personal />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path='/my-account' element={<MyAccount/>} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
+
+export default App;
